16ec26d1d2f42e148739920da82042bb5326bcf4
Cesar Blasco <cesarblasco50@hotmail.com>
2020-02-14 18:07:45 -0400

    Apply prettier to ts / html files, change placeholder in list input

--- src/components/repository-detail/repository-detail.component.ts
+++ src/components/repository-detail/repository-detail.component.ts
@@ -1,69 +1,87 @@
-import { GithubService } from '../../services/github.service';
-import { TableModificationService } from '../../services/table-modification.service';
+import { GithubService } from "../../services/github.service";
+import { TableModificationService } from "../../services/table-modification.service";
 
 class RepositoryDetailController implements ng.IController {
-    public sortAscent: boolean;
-    public currentSortedProperty: string;
-    private repository: any;
-    private contributors: any;
-    private contributorsCopy: any;
-    private filterString: string;
-    constructor(private $stateParams, private githubService: GithubService,
-        private tableModificationService: TableModificationService) {
-    }
+  public sortAscent: boolean;
+  public currentSortedProperty: string;
+  private repository: any;
+  private contributors: any;
+  private contributorsCopy: any;
+  private filterString: string;
+  constructor(
+    private $stateParams,
+    private githubService: GithubService,
+    private tableModificationService: TableModificationService
+  ) {}
 
-    public $onInit() {
-        this.githubService.getRepositoryById(this.$stateParams.id).then((response) => {
-            this.repository = response.data;
-            this.githubService.getRepositoryContributors(this.repository.owner.login, this.repository.name)
-                .then((responseContributors) => {
-                    this.contributors = responseContributors.data;
-                    this.contributorsCopy = this.contributors;
-                    this.contributors.map((contributor) => {
-                        this.githubService.getUserByUsername(contributor.login).then((responseUsername) => {
-                            const user: any = responseUsername.data;
-                            contributor.email = user.email;
-                        });
-                    });
+  public $onInit() {
+    this.githubService
+      .getRepositoryById(this.$stateParams.id)
+      .then(response => {
+        this.repository = response.data;
+        this.githubService
+          .getRepositoryContributors(
+            this.repository.owner.login,
+            this.repository.name
+          )
+          .then(responseContributors => {
+            this.contributors = responseContributors.data;
+            this.contributorsCopy = this.contributors;
+            this.contributors.map(contributor => {
+              this.githubService
+                .getUserByUsername(contributor.login)
+                .then(responseUsername => {
+                  const user: any = responseUsername.data;
+                  contributor.email = user.email;
                 });
-        });
-    }
+            });
+          });
+      });
+  }
 
-    public sort(sortBy: string) {
-        const { sortedData, sortAscent, sortedProperty } = this.tableModificationService
-            .sortData(this.contributors, sortBy);
-        this.contributors = sortedData;
-        this.currentSortedProperty = sortedProperty;
-        this.sortAscent = sortAscent;
-    }
+  public sort(sortBy: string) {
+    const {
+      sortedData,
+      sortAscent,
+      sortedProperty
+    } = this.tableModificationService.sortData(this.contributors, sortBy);
+    this.contributors = sortedData;
+    this.currentSortedProperty = sortedProperty;
+    this.sortAscent = sortAscent;
+  }
 
-    public filter(filterString: string, attribute: string) {
-        if (filterString.length < 1) {
-            this.contributors = this.contributorsCopy;
-        }
-        this.contributors = this.tableModificationService.filterData(this.contributors, filterString, attribute);
+  public filter(filterString: string, attribute: string) {
+    if (filterString.length < 1) {
+      this.contributors = this.contributorsCopy;
     }
+    this.contributors = this.tableModificationService.filterData(
+      this.contributors,
+      filterString,
+      attribute
+    );
+  }
 
-    public isCurrentSort(property: string) {
-        return this.sortAscent && this.currentSortedProperty === property;
-    }
+  public isCurrentSort(property: string) {
+    return this.sortAscent && this.currentSortedProperty === property;
+  }
 
-    public getSortIcon(property: string) {
-        if (this.currentSortedProperty === property) {
-            return this.sortAscent ? 'fa fa-sort-asc' : 'fa fa-sort-desc';
-        } else {
-            return 'fa fa-sort';
-        }
+  public getSortIcon(property: string) {
+    if (this.currentSortedProperty === property) {
+      return this.sortAscent ? "fa fa-sort-asc" : "fa fa-sort-desc";
+    } else {
+      return "fa fa-sort";
     }
+  }
 }
 
 // tslint:disable-next-line:max-classes-per-file
 export class RepositoryDetailComponent implements ng.IComponentOptions {
-    public static NAME: string = 'repositoryDetailView';
-    public controller: any;
-    public templateUrl: any;
-    constructor() {
-        this.controller = RepositoryDetailController;
-        this.templateUrl = './src/components/repository-detail/repository-detail.component.html';
-    }
+  public static NAME: string = "repositoryDetailView";
+  public controller: any;
+  public templateUrl: any;
+  constructor() {
+    this.controller = RepositoryDetailController;
+    this.templateUrl =
+      "./src/components/repository-detail/repository-detail.component.html";
+  }
 }
--- src/components/repository-list/repository-list.component.ts
+++ src/components/repository-list/repository-list.component.ts
@@ -1,93 +1,110 @@
-import { GithubService } from '../../services/github.service';
-import { SearchStateService } from '../../services/search-state.service';
-import { TableModificationService } from '../../services/table-modification.service';
+import { GithubService } from "../../services/github.service";
+import { SearchStateService } from "../../services/search-state.service";
+import { TableModificationService } from "../../services/table-modification.service";
 
 class RepositoryListController implements ng.IController {
-    public loading: boolean;
-    public sortAscent: boolean;
-    public currentSortedProperty: string;
-    private searchTerm: string;
-    private userRepositories: any;
-    private userRepositoriesCopy: any;
-    private currentAvatarURL: string;
-    private errorMessage: string;
-    private filterString: string;
+  public loading: boolean;
+  public sortAscent: boolean;
+  public currentSortedProperty: string;
+  private searchTerm: string;
+  private userRepositories: any;
+  private userRepositoriesCopy: any;
+  private currentAvatarURL: string;
+  private errorMessage: string;
+  private filterString: string;
 
-    constructor(private githubService: GithubService, private searchStateService: SearchStateService,
-                private tableModificationService: TableModificationService) {
+  constructor(
+    private githubService: GithubService,
+    private searchStateService: SearchStateService,
+    private tableModificationService: TableModificationService
+  ) {}
+
+  public $onInit() {
+    const lastSearch = this.searchStateService.getLastSearch();
+    this.searchTerm = lastSearch.searchTerm;
+    this.userRepositories = lastSearch.searchData;
+    this.currentAvatarURL = lastSearch.avatarURL;
+    this.filterString = lastSearch.filterString;
+  }
+
+  public searchRepositories() {
+    if (this.searchTerm !== "") {
+      this.loading = true;
+      this.githubService
+        .getAllRepositoriesByUsername(this.searchTerm)
+        .then(response => {
+          this.userRepositories = response.data;
+          this.userRepositoriesCopy = this.userRepositories;
+          this.currentAvatarURL = this.userRepositories[0].owner.avatar_url;
+          this.errorMessage = "";
+          this.loading = false;
+        })
+        .catch(() => {
+          this.currentAvatarURL = null;
+          this.userRepositories = null;
+          this.errorMessage =
+            this.searchTerm === ""
+              ? ""
+              : "No se encontraron resultados por ese nombre de usuario";
+          this.loading = false;
+        });
+    } else {
+      this.errorMessage = "";
     }
+  }
 
-    public $onInit() {
-        const lastSearch = this.searchStateService.getLastSearch();
-        this.searchTerm = lastSearch.searchTerm;
-        this.userRepositories = lastSearch.searchData;
-        this.currentAvatarURL = lastSearch.avatarURL;
-        this.filterString = lastSearch.filterString;
+  public saveLastSearch() {
+    this.searchStateService.saveSearch(
+      this.searchTerm,
+      this.userRepositories,
+      this.currentAvatarURL,
+      this.filterString
+    );
+  }
+
+  public sort(sortBy: string) {
+    const {
+      sortedData,
+      sortAscent,
+      sortedProperty
+    } = this.tableModificationService.sortData(this.userRepositories, sortBy);
+    this.userRepositories = sortedData;
+    this.currentSortedProperty = sortedProperty;
+    this.sortAscent = sortAscent;
+  }
+
+  public filter(filterString: string, attribute: string) {
+    if (filterString.length < 1) {
+      this.userRepositories = this.userRepositoriesCopy;
     }
+    this.userRepositories = this.tableModificationService.filterData(
+      this.userRepositories,
+      filterString,
+      attribute
+    );
+  }
 
-    public searchRepositories() {
-        if (this.searchTerm !== '') {
-            this.loading = true;
-            this.githubService.getAllRepositoriesByUsername(this.searchTerm).then((response) => {
-                this.userRepositories = response.data;
-                this.userRepositoriesCopy = this.userRepositories;
-                this.currentAvatarURL = this.userRepositories[0].owner.avatar_url;
-                this.errorMessage = '';
-                this.loading = false;
-            }).catch(() => {
-                this.currentAvatarURL = null;
-                this.userRepositories = null;
-                this.errorMessage = this.searchTerm === '' ?
-                                                        '' : 'No se encontraron resultados por ese nombre de usuario';
-                this.loading = false;
-            });
-        } else {
-            this.errorMessage = '';
-        }
+  public isCurrentSort(property: string) {
+    return this.sortAscent && this.currentSortedProperty === property;
+  }
+
+  public getSortIcon(property: string) {
+    if (this.currentSortedProperty === property) {
+      return this.sortAscent ? "fa fa-sort-asc" : "fa fa-sort-desc";
+    } else {
+      return "fa fa-sort";
     }
-
-    public saveLastSearch() {
-        this.searchStateService.saveSearch(this.searchTerm, this.userRepositories,
-                                        this.currentAvatarURL, this.filterString);
-    }
-
-    public sort(sortBy: string) {
-        const { sortedData, sortAscent, sortedProperty } = this.tableModificationService
-                                                               .sortData(this.userRepositories, sortBy);
-        this.userRepositories = sortedData;
-        this.currentSortedProperty = sortedProperty;
-        this.sortAscent = sortAscent;
-    }
-
-    public filter(filterString: string, attribute: string) {
-        if (filterString.length < 1) {
-            this.userRepositories = this.userRepositoriesCopy;
-        }
-        this.userRepositories = this.tableModificationService.filterData(this.userRepositories,
-                                                                         filterString, attribute);
-    }
-
-    public isCurrentSort(property: string) {
-        return this.sortAscent && this.currentSortedProperty === property;
-    }
-
-    public getSortIcon(property: string) {
-        if (this.currentSortedProperty === property) {
-            return this.sortAscent ? 'fa fa-sort-asc' : 'fa fa-sort-desc';
-        } else {
-            return 'fa fa-sort';
-        }
-    }
-
+  }
 }
 
 // tslint:disable-next-line:max-classes-per-file
 export class RepositoryListComponent implements ng.IComponentOptions {
-    public static NAME: string = 'repositoryListView';
-    public controller: any;
-    public templateUrl: any;
-    constructor() {
-       this.controller = RepositoryListController;
-       this.templateUrl = './src/components/repository-list/repository-list.component.html';
-    }
+  public static NAME: string = "repositoryListView";
+  public controller: any;
+  public templateUrl: any;
+  constructor() {
+    this.controller = RepositoryListController;
+    this.templateUrl =
+      "./src/components/repository-list/repository-list.component.html";
+  }
 }
